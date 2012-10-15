2b1c5c8643562d689d6c6aad3d5750a99ee9b8a6
Sitapati <sitapati@worldsankirtan.net>
2012-10-15 18:02:15 +1100

    Added isContentSpec method

--- pressgangccms.js
+++ pressgangccms.js
@@ -11,18 +11,19 @@ var rest = require('restler');
 var eventEmitter = require('events').EventEmitter;
 var util = require('util');
 
+var constants = { contentSpecTagID: 268 }
+
 module.exports.PressGangCCMS = PressGangCCMS;
 
-function PressGangCCMS (url, options){
-    this.url = url;   
+function PressGangCCMS(url, options){
+    this.url = url || '';   
     this.settings = {
         username: '',
         authmethod: '',
         authtoken: '',
-		restver: 1
+        restver: 1
     }
-
-	util.inherits(PressGangCCMS, eventEmitter); 
+ 
     if (options && options.key){
         for (var i in options) {
             if (options.hasOwnProperty(i)) {
@@ -30,6 +31,12 @@ function PressGangCCMS (url, options){
             }
         }
     }
+}
+
+util.inherits(PressGangCCMS, eventEmitter);
+
+PressGangCCMS.prototype.log = function(msg, lvl) {
+    if (this.settings.loglevel > lvl) console.log(msg);
 }
 
 PressGangCCMS.prototype.get = function (key) {
@@ -44,21 +51,52 @@ PressGangCCMS.prototype.set = function (key, value) {
 };
 
 
+PressGangCCMS.prototype.isContentSpec = function(topic_id, cb)
+{
+    this.getTopicData(topic_id, 'topic-tags', function(err, result){ 
+        var is_spec = false;
+        if (result.length > 0){
+            for (var i = 0; i < result.length; i ++){
+                if (result[i].item.id == constants.contentSpecTagID) is_spec = true;
+            }
+        }
+        cb(err, is_spec);
+    });
+}
+
 PressGangCCMS.prototype.getTopicData = function(topic_id, data_request, cb)
 {
-    if (this.url && (this.url !== ''))
+    this.log(this.settings,2);
+    if (this.url)
     {
         var requestpath;
 
         // assemble the request path from the url, data_request, and topic id
-        switch(data_request){
-         
-         case 'xml':
-           requestpath = '/seam/resource/rest/1/topic/get/xml/' + topic_id +'/xml';
-           break;
-           
+        switch (this.settings.restver){
+            case 1: // REST API v.1
+                switch(data_request){ // These are the different data requests we support
+                
+                    // 'xml': Return the topic xml (plain text in the case of a Content Spec
+                    case 'xml':
+                        requestpath = '/seam/resource/rest/1/topic/get/xml/' + topic_id +'/xml';
+                        break;
+                   
+                    // 'json-topic': Return the json representation of a topic
+                    case 'json-topic':
+                        requestpath = '/seam/resource/rest/1/topic/get/json/' + topic_id;
+                        break;
+                
+                    // 'topic-tags': Return an expanded collection of tags
+                    case 'topic-tags':
+                        requestpath = '/seam/resource/rest/1/topic/get/json/' + topic_id + '?expand='
+                        requestpath = requestpath + encodeURIComponent('{"branches":[{"trunk":{"name":"tags"}}]}'); 
+                        break;
+            }
+            break;
         }
-
+        
+        this.log(this.url + requestpath, 2);
+        
         rest.get(this.url + requestpath).on('complete', function(result){
             if (result instanceof Error)
             {
@@ -66,7 +104,17 @@ PressGangCCMS.prototype.getTopicData = function(topic_id, data_request, cb)
             }
             else
             {
-                cb(null, result);
+                var parsed_result = result;
+                
+                // Will need to be switched on REST ver in the future also, 
+                // or perhaps a single switch that encompasses the entire pathway
+                // including the REST call
+                switch(data_request){
+                    case 'topic-tags':
+                        parsed_result = result.tags.items; 
+                }
+                
+                cb(null, parsed_result);
             }
         });
     }
@@ -75,3 +123,4 @@ PressGangCCMS.prototype.getTopicData = function(topic_id, data_request, cb)
         cb(new Error('No server URL specified'));
     }
 }
+
